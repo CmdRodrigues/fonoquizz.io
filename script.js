dbecf5add1ffbc270d580d48062b93dd41f6a2e6
CmdRodrigues <lcleiton538@gmail.com>
2025-04-06 21:40:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const quizData = [
     {
         question: "Qual das alternativas apresenta um marco típico do desenvolvimento da linguagem aos 2 anos de idade?",
-        options 1: ["Produz frases complexas com sujeito e predicado", "Utiliza entre 50 a 200 palavras e começa a formar frases de duas palavras", "Nomeia cores primárias corretamente", "Utiliza pronomes pessoais com consistência"],
+        options 1: ["A) Produz frases complexas com sujeito e predicado", "B) Utiliza entre 50 a 200 palavras e começa a formar frases de duas palavras", "C) Nomeia cores primárias corretamente", "D) Utiliza pronomes pessoais com consistência"],
         answer: 2
     },
     {
         question 2: "Na audiometria tonal limiar, a perda auditiva do tipo condutiva se caracteriza por:",
-        options: ["Alterações nas vias centrais auditivas com limiares normais", "Presença de gap aéreo-ósseo com via óssea normal", "Perda auditiva simétrica e neurossensorial", "Limiares auditivos normais com queixa de zumbido"],
+        options: ["A) Alterações nas vias centrais auditivas com limiares normais", "B) Presença de gap aéreo-ósseo com via óssea normal", "C) Perda auditiva simétrica e neurossensorial", "D) Limiares auditivos normais com queixa de zumbido"],
         answer: 0
     },
     {
         question 3: "A respiração oral pode provocar, EXCETO",
-        options: ["Mordida aberta anterior", "Hipotonia dos músculos orofaciais", "Aumento da pressão intraoral", "Alterações na postura da língua em repouso"],
+        options: ["A) Mordida aberta anterior", "B) Hipotonia dos músculos orofaciais", "C) Aumento da pressão intraoral", "D) Alterações na postura da língua em repouso"],
         answer: 2
     }
 ];
